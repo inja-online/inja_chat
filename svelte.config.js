715147763e759de31c2caa7206efc0382d11aa20
Alireza SadraiiRad <sadraiiali@gmail.com>
2025-06-08 15:51:24 +0200

    feat: update project configuration and add deployment support

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,24 @@ import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	preprocess: [
+		vitePreprocess(), 
+		mdsvex()
+],
+	kit: {
+		adapter: adapter({
+			config: undefined,
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			}
+		}),
+		prerender: {
+			entries: ["/"],
+		},
+		csp: { mode: "auto" },
+	},
 	extensions: ['.svelte', '.svx']
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		devtoolsJson(),
+		tailwindcss(),
+		sveltekit()
+	]
 });
